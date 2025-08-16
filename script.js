b65a92305e803e03855419951a6935672efc9f81
fdiw <116688306+fd-iw@users.noreply.github.com>
2025-08-16 07:20:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Card interactive tilt/indent effect
+// Card interactive indent/tilt effect
 document.querySelectorAll(".card").forEach(card => {
   card.addEventListener("mousemove", e => {
     const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left; 
+    const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    const rotateX = ((y / rect.height) - 0.5) * -15; 
+    const rotateX = ((y / rect.height) - 0.5) * -15;
     const rotateY = ((x / rect.width) - 0.5) * 15;
 
     card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
